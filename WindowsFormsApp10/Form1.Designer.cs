52f68a54384ba61a202555546e8ed93c9e309f33
UgurCan Yuksel <ugurcanyksl@outlook.com>
2021-01-17 01:49:26 +0300

    ready

---
+++ WindowsFormsApp10/Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace WindowsFormsApp10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pbAt1 = new System.Windows.Forms.PictureBox();
+            this.pbAt2 = new System.Windows.Forms.PictureBox();
+            this.pbAt3 = new System.Windows.Forms.PictureBox();
+            this.lblFinish = new System.Windows.Forms.Label();
+            this.btnBaslat = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblSpiker = new System.Windows.Forms.Label();
+            this.numeric1 = new System.Windows.Forms.NumericUpDown();
+            this.numeric2 = new System.Windows.Forms.NumericUpDown();
+            this.numeric3 = new System.Windows.Forms.NumericUpDown();
+            this.lblYüzOtuz = new System.Windows.Forms.Label();
+            this.lblYüzElli = new System.Windows.Forms.Label();
+            this.lblYüzSeksen = new System.Windows.Forms.Label();
+            this.btnYatir1 = new System.Windows.Forms.Button();
+            this.btnYatir2 = new System.Windows.Forms.Button();
+            this.btnYatir3 = new System.Windows.Forms.Button();
+            this.lblBakiye = new System.Windows.Forms.Label();
+            this.lblYatırılanPara = new System.Windows.Forms.Label();
+            this.listBahis = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAt1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAt2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAt3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(3, 174);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1133, 22);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Location = new System.Drawing.Point(3, 302);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(1133, 22);
+            this.label2.TabIndex = 0;
+            // 
+            // pbAt1
+            // 
+            this.pbAt1.BackColor = System.Drawing.Color.Transparent;
+            this.pbAt1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbAt1.Image = global::WindowsFormsApp10.Properties.Resources.ganyan;
+            this.pbAt1.Location = new System.Drawing.Point(28, 79);
+            this.pbAt1.Margin = new System.Windows.Forms.Padding(4);
+            this.pbAt1.Name = "pbAt1";
+            this.pbAt1.Size = new System.Drawing.Size(133, 75);
+            this.pbAt1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbAt1.TabIndex = 1;
+            this.pbAt1.TabStop = false;
+            // 
+            // pbAt2
+            // 
+            this.pbAt2.BackColor = System.Drawing.Color.Transparent;
+            this.pbAt2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbAt2.Image = global::WindowsFormsApp10.Properties.Resources._5139947333409;
+            this.pbAt2.Location = new System.Drawing.Point(28, 206);
+            this.pbAt2.Margin = new System.Windows.Forms.Padding(4);
+            this.pbAt2.Name = "pbAt2";
+            this.pbAt2.Size = new System.Drawing.Size(133, 75);
+            this.pbAt2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbAt2.TabIndex = 1;
+            this.pbAt2.TabStop = false;
+            // 
+            // pbAt3
+            // 
+            this.pbAt3.BackColor = System.Drawing.Color.Transparent;
+            this.pbAt3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbAt3.Image = global::WindowsFormsApp10.Properties.Resources.hrs;
+            this.pbAt3.Location = new System.Drawing.Point(28, 340);
+            this.pbAt3.Margin = new System.Windows.Forms.Padding(4);
+            this.pbAt3.Name = "pbAt3";
+            this.pbAt3.Size = new System.Drawing.Size(133, 75);
+            this.pbAt3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbAt3.TabIndex = 1;
+            this.pbAt3.TabStop = false;
+            // 
+            // lblFinish
+            // 
+            this.lblFinish.BackColor = System.Drawing.Color.IndianRed;
+            this.lblFinish.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFinish.ForeColor = System.Drawing.Color.White;
+            this.lblFinish.Location = new System.Drawing.Point(1105, 79);
+            this.lblFinish.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblFinish.Name = "lblFinish";
+            this.lblFinish.Size = new System.Drawing.Size(31, 386);
+            this.lblFinish.TabIndex = 2;
+            this.lblFinish.Text = "FİNİSH";
+            this.lblFinish.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.Location = new System.Drawing.Point(13, 524);
+            this.btnBaslat.Margin = new System.Windows.Forms.Padding(4);
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(111, 33);
+            this.btnBaslat.TabIndex = 3;
+            this.btnBaslat.Text = "Başlat";
+            this.btnBaslat.UseVisualStyleBackColor = true;
+            this.btnBaslat.Click += new System.EventHandler(this.BtnBaslat_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // lblSpiker
+            // 
+            this.lblSpiker.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblSpiker.Location = new System.Drawing.Point(724, 644);
+            this.lblSpiker.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSpiker.Name = "lblSpiker";
+            this.lblSpiker.Size = new System.Drawing.Size(403, 36);
+            this.lblSpiker.TabIndex = 4;
+            // 
+            // numeric1
+            // 
+            this.numeric1.Location = new System.Drawing.Point(261, 639);
+            this.numeric1.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.numeric1.Name = "numeric1";
+            this.numeric1.Size = new System.Drawing.Size(58, 22);
+            this.numeric1.TabIndex = 5;
+            // 
+            // numeric2
+            // 
+            this.numeric2.Location = new System.Drawing.Point(342, 639);
+            this.numeric2.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.numeric2.Name = "numeric2";
+            this.numeric2.Size = new System.Drawing.Size(58, 22);
+            this.numeric2.TabIndex = 5;
+            // 
+            // numeric3
+            // 
+            this.numeric3.Location = new System.Drawing.Point(420, 639);
+            this.numeric3.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.numeric3.Name = "numeric3";
+            this.numeric3.Size = new System.Drawing.Size(58, 22);
+            this.numeric3.TabIndex = 5;
+            // 
+            // lblYüzOtuz
+            // 
+            this.lblYüzOtuz.AutoSize = true;
+            this.lblYüzOtuz.Location = new System.Drawing.Point(258, 605);
+            this.lblYüzOtuz.Name = "lblYüzOtuz";
+            this.lblYüzOtuz.Size = new System.Drawing.Size(36, 17);
+            this.lblYüzOtuz.TabIndex = 6;
+            this.lblYüzOtuz.Text = "1,30";
+            // 
+            // lblYüzElli
+            // 
+            this.lblYüzElli.AutoSize = true;
+            this.lblYüzElli.Location = new System.Drawing.Point(339, 605);
+            this.lblYüzElli.Name = "lblYüzElli";
+            this.lblYüzElli.Size = new System.Drawing.Size(36, 17);
+            this.lblYüzElli.TabIndex = 6;
+            this.lblYüzElli.Text = "1,50";
+            // 
+            // lblYüzSeksen
+            // 
+            this.lblYüzSeksen.AutoSize = true;
+            this.lblYüzSeksen.Location = new System.Drawing.Point(417, 605);
+            this.lblYüzSeksen.Name = "lblYüzSeksen";
+            this.lblYüzSeksen.Size = new System.Drawing.Size(36, 17);
+            this.lblYüzSeksen.TabIndex = 6;
+            this.lblYüzSeksen.Text = "1,80";
+            // 
+            // btnYatir1
+            // 
+            this.btnYatir1.Location = new System.Drawing.Point(261, 667);
+            this.btnYatir1.Name = "btnYatir1";
+            this.btnYatir1.Size = new System.Drawing.Size(58, 23);
+            this.btnYatir1.TabIndex = 7;
+            this.btnYatir1.Text = "Yatır";
+            this.btnYatir1.UseVisualStyleBackColor = true;
+            this.btnYatir1.Click += new System.EventHandler(this.btnYatir1_Click);
+            // 
+            // btnYatir2
+            // 
+            this.btnYatir2.Location = new System.Drawing.Point(342, 667);
+            this.btnYatir2.Name = "btnYatir2";
+            this.btnYatir2.Size = new System.Drawing.Size(58, 23);
+            this.btnYatir2.TabIndex = 7;
+            this.btnYatir2.Text = "Yatır";
+            this.btnYatir2.UseVisualStyleBackColor = true;
+            this.btnYatir2.Click += new System.EventHandler(this.btnYatir2_Click);
+            // 
+            // btnYatir3
+            // 
+            this.btnYatir3.Location = new System.Drawing.Point(420, 667);
+            this.btnYatir3.Name = "btnYatir3";
+            this.btnYatir3.Size = new System.Drawing.Size(58, 23);
+            this.btnYatir3.TabIndex = 7;
+            this.btnYatir3.Text = "Yatır";
+            this.btnYatir3.UseVisualStyleBackColor = true;
+            this.btnYatir3.Click += new System.EventHandler(this.btnYatir3_Click);
+            // 
+            // lblBakiye
+            // 
+            this.lblBakiye.AutoSize = true;
+            this.lblBakiye.Location = new System.Drawing.Point(537, 644);
+            this.lblBakiye.Name = "lblBakiye";
+            this.lblBakiye.Size = new System.Drawing.Size(53, 17);
+            this.lblBakiye.TabIndex = 8;
+            this.lblBakiye.Text = "500 TL";
+            // 
+            // lblYatırılanPara
+            // 
+            this.lblYatırılanPara.AutoSize = true;
+            this.lblYatırılanPara.Location = new System.Drawing.Point(724, 619);
+            this.lblYatırılanPara.Name = "lblYatırılanPara";
+            this.lblYatırılanPara.Size = new System.Drawing.Size(93, 17);
+            this.lblYatırılanPara.TabIndex = 9;
+            this.lblYatırılanPara.Text = "Yatırılan Para";
+            // 
+            // listBahis
+            // 
+            this.listBahis.FormattingEnabled = true;
+            this.listBahis.ItemHeight = 16;
+            this.listBahis.Location = new System.Drawing.Point(951, 463);
+            this.listBahis.Name = "listBahis";
+            this.listBahis.Size = new System.Drawing.Size(142, 132);
+            this.listBahis.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1141, 722);
+            this.Controls.Add(this.listBahis);
+            this.Controls.Add(this.lblYatırılanPara);
+            this.Controls.Add(this.lblBakiye);
+            this.Controls.Add(this.btnYatir3);
+            this.Controls.Add(this.btnYatir2);
+            this.Controls.Add(this.btnYatir1);
+            this.Controls.Add(this.lblYüzSeksen);
+            this.Controls.Add(this.lblYüzElli);
+            this.Controls.Add(this.lblYüzOtuz);
+            this.Controls.Add(this.numeric3);
+            this.Controls.Add(this.numeric2);
+            this.Controls.Add(this.numeric1);
+            this.Controls.Add(this.lblSpiker);
+            this.Controls.Add(this.btnBaslat);
+            this.Controls.Add(this.lblFinish);
+            this.Controls.Add(this.pbAt3);
+            this.Controls.Add(this.pbAt2);
+            this.Controls.Add(this.pbAt1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbAt1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAt2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAt3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pbAt1;
+        private System.Windows.Forms.PictureBox pbAt2;
+        private System.Windows.Forms.PictureBox pbAt3;
+        private System.Windows.Forms.Label lblFinish;
+        private System.Windows.Forms.Button btnBaslat;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblSpiker;
+        private System.Windows.Forms.NumericUpDown numeric1;
+        private System.Windows.Forms.NumericUpDown numeric2;
+        private System.Windows.Forms.NumericUpDown numeric3;
+        private System.Windows.Forms.Label lblYüzOtuz;
+        private System.Windows.Forms.Label lblYüzElli;
+        private System.Windows.Forms.Label lblYüzSeksen;
+        private System.Windows.Forms.Button btnYatir1;
+        private System.Windows.Forms.Button btnYatir2;
+        private System.Windows.Forms.Button btnYatir3;
+        private System.Windows.Forms.Label lblBakiye;
+        private System.Windows.Forms.Label lblYatırılanPara;
+        private System.Windows.Forms.ListBox listBahis;
+    }
+}
+
---
+++ WindowsFormsApp10/Form1.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+        int yatırılanPara, kazanan, at1, at2, at3;
+        double yüzOtuz, yüzElli, yüzSeksen, numericDeger, bakiye;
+        private void BtnBaslat_Click(object sender, EventArgs e)
+        {
+
+            timer1.Start();
+
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            switch (kazanan)
+            {
+                case 1:
+                    pbAt1.Left += rnd.Next(30, 50);
+                    pbAt2.Left += rnd.Next(10, 50);
+                    pbAt3.Left += rnd.Next(10, 50);
+                    break;
+
+                case 2:
+                    pbAt1.Left += rnd.Next(10, 50);
+                    pbAt2.Left += rnd.Next(30, 50);
+                    pbAt3.Left += rnd.Next(10, 50);
+                    break;
+
+                case 3:
+                    pbAt1.Left += rnd.Next(10, 50);
+                    pbAt2.Left += rnd.Next(10, 50);
+                    pbAt3.Left += rnd.Next(30, 50);
+                    break;
+            }
+
+
+            if (pbAt1.Right >= pbAt2.Right && pbAt1.Right >= pbAt3.Right)
+            {
+                lblSpiker.Text = "1. At önde gidiyor";
+            }
+            else if (pbAt2.Right > pbAt1.Right && pbAt2.Right > pbAt3.Right)
+            {
+                lblSpiker.Text = "2. At önde gidiyor";
+            }
+            else if (pbAt3.Right > pbAt1.Right && pbAt3.Right > pbAt2.Right)
+            {
+                lblSpiker.Text = "3. At önde gidiyor";
+            }
+
+            if (pbAt1.Right >= lblFinish.Left)
+            {
+                timer1.Stop();
+                DialogResult dr = MessageBox.Show("1. At kazandı\ntekrar oynamak ister misin", "oyun bitti", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+                
+
+                if (dr == DialogResult.Yes)
+                {
+                    if (bahisOynanaAt == 1) 
+
+                    {
+                        
+                        bakiye += yatırılanPara * 1.30;
+                        lblBakiye.Text = bakiye.ToString("C");
+                        listBahis.Items.Add("Kazanılan Para :" + (yatırılanPara * 1.30));
+                        yatırılanPara = 0;
+
+                        
+                    }
+
+                    pbAt1.Left = 12;
+                    pbAt2.Left = 12;
+                    pbAt3.Left = 12;
+
+                    btnYatir3.Enabled = true;
+                    numeric3.Enabled = true;
+                    btnYatir2.Enabled = true;
+                    numeric2.Enabled = true;
+                    btnYatir1.Enabled = true;
+                    numeric1.Enabled = true;
+
+
+
+                    renk = rnd.Next(1, 4);
+                    switch (renk)
+                    {
+                        case 1:
+                            this.BackColor = Color.Blue;
+                            kazanan = 1;
+                            break;
+                        case 2:
+                            this.BackColor = Color.Sienna;
+                            kazanan = 2;
+                            break;
+                        case 3:
+                            this.BackColor = Color.Green;
+                            kazanan = 3;
+                            break;
+                    }
+
+
+
+                }
+                else
+                {
+
+                }
+            }
+
+            else if (pbAt2.Right >= lblFinish.Left)
+            {
+                timer1.Stop();
+                DialogResult dr = MessageBox.Show("2. At kazandı\ntekrar oynamak ister misin", "oyun bitti", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+                if (dr == DialogResult.Yes)
+                {
+                    if (bahisOynanaAt==2)
+                    {
+                        bakiye += yatırılanPara * 1.50;
+                        lblBakiye.Text = bakiye.ToString("C");
+                        listBahis.Items.Add("Kazanılan Para :" + yatırılanPara * 1.50);
+                        yatırılanPara = 0;
+
+                        
+                    }
+
+                    pbAt1.Left = 12;
+                    pbAt2.Left = 12;
+                    pbAt3.Left = 12;
+
+                    btnYatir3.Enabled = true;
+                    numeric3.Enabled = true;
+                    btnYatir2.Enabled = true;
+                    numeric2.Enabled = true;
+                    btnYatir1.Enabled = true;
+                    numeric1.Enabled = true;
+
+                    renk = rnd.Next(1, 4);
+                    switch (renk)
+                    {
+                        case 1:
+                            this.BackColor = Color.Blue;
+                            kazanan = 1;
+                            break;
+                        case 2:
+                            this.BackColor = Color.Sienna;
+                            kazanan = 2;
+                            break;
+                        case 3:
+                            this.BackColor = Color.Green;
+                            kazanan = 3;
+                            break;
+                    }
+                }
+            }
+
+            else if (pbAt3.Right >= lblFinish.Left)
+            {
+                timer1.Stop();
+                DialogResult dr = MessageBox.Show("3. At kazandı\ntekrar oynamak ister misin", "oyun bitti", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+                if (dr == DialogResult.Yes)
+                {
+
+                    if (bahisOynanaAt == 3)
+                    {
+                        bakiye += yatırılanPara * 1.80;
+                        lblBakiye.Text = bakiye.ToString("C");
+                        listBahis.Items.Add("Kazanılan Para :" + yatırılanPara * 1.80);
+                        yatırılanPara = 0;
+
+                        
+                    }
+
+                    pbAt1.Left = 12;
+                    pbAt2.Left = 12;
+                    pbAt3.Left = 12;
+
+                    btnYatir3.Enabled = true;
+                    numeric3.Enabled = true;
+                    btnYatir2.Enabled = true;
+                    numeric2.Enabled = true;
+                    btnYatir1.Enabled = true;
+                    numeric1.Enabled = true;
+
+                    renk = rnd.Next(1, 4);
+                    switch (renk)
+                    {
+                        case 1:
+                            this.BackColor = Color.Blue;
+                            kazanan = 1;
+                            break;
+                        case 2:
+                            this.BackColor = Color.Sienna;
+                            kazanan = 2;
+                            break;
+                        case 3:
+                            this.BackColor = Color.Green;
+                            kazanan = 3;
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void btnYatir3_Click(object sender, EventArgs e)
+        {
+            bahisOynanaAt = 3;
+            yatırılanPara = int.Parse(numeric3.Value.ToString());
+
+            //yüzSeksen = Convert.ToDouble(lblYüzSeksen.Text);
+            numericDeger = Convert.ToDouble(numeric3.Value);
+            bakiye = Convert.ToDouble(lblBakiye.Text);
+            //lblYatırılanPara.Text = (yüzSeksen * numericDeger).ToString();
+            lblYatırılanPara.Text = numericDeger.ToString();
+
+            listBahis.Items.Add("Çekilen Para :" + lblYatırılanPara.Text);
+            lblBakiye.Text = (bakiye - numericDeger).ToString();
+            btnYatir3.Enabled = false;
+            numeric3.Enabled = false;
+            btnYatir2.Enabled = false;
+            numeric2.Enabled = false;
+            btnYatir1.Enabled = false;
+            numeric1.Enabled = false;
+        }
+
+        private void btnYatir2_Click(object sender, EventArgs e)
+        {
+            bahisOynanaAt = 2;
+            yatırılanPara = int.Parse(numeric2.Value.ToString());
+
+            //yüzElli = Convert.ToDouble(lblYüzElli.Text);
+            numericDeger = Convert.ToDouble(numeric2.Value);
+            bakiye = Convert.ToDouble(lblBakiye.Text);
+            //lblYatırılanPara.Text = (yüzElli * numericDeger).ToString();
+            lblYatırılanPara.Text = numericDeger.ToString();
+
+            listBahis.Items.Add("Çekilen Para :" + lblYatırılanPara.Text);
+            lblBakiye.Text = (bakiye - numericDeger).ToString();
+            btnYatir2.Enabled = false;
+            numeric2.Enabled = false;
+            btnYatir3.Enabled = false;
+            numeric3.Enabled = false;
+            btnYatir1.Enabled = false;
+            numeric1.Enabled = false;
+        }
+        int bahisOynanaAt;
+        private void btnYatir1_Click(object sender, EventArgs e)
+        {
+            bahisOynanaAt = 1;
+            yatırılanPara = int.Parse(numeric1.Value.ToString());
+            //yüzOtuz = Convert.ToDouble(lblYüzOtuz.Text);
+            numericDeger = Convert.ToDouble(numeric1.Value);
+            bakiye = Convert.ToDouble(lblBakiye.Text);
+            //lblYatırılanPara.Text = (yüzOtuz * numericDeger).ToString();
+            lblYatırılanPara.Text = numericDeger.ToString();
+
+            listBahis.Items.Add("Çekilen Para :" + lblYatırılanPara.Text);
+            lblBakiye.Text = (bakiye - numericDeger).ToString();
+            btnYatir1.Enabled = false;
+            numeric1.Enabled = false;
+            btnYatir2.Enabled = false;
+            numeric2.Enabled = false;
+            btnYatir3.Enabled = false;
+            numeric3.Enabled = false;
+
+
+
+        }
+        int renk;
+        private void Form1_Load(object sender, EventArgs e)
+        {   //rastgele 1-3 arası bir deger atadık ve  atadığımız değere göre arkaplan rengini değiştirdik.
+            renk = rnd.Next(1, 4);
+            switch (renk)
+            {
+                case 1:
+                    this.BackColor = Color.Blue;
+                    kazanan = 1;
+                    break;
+                case 2:
+                    this.BackColor = Color.Sienna;
+                    kazanan = 2;
+                    break;
+                case 3:
+                    this.BackColor = Color.Green;
+                    kazanan = 3;
+                    break;
+
+            }
+
+            //mesela şey 1. at kazandı ama hesaba para yatma işi o çok hata verdi o zaman konuşmuştuk olmadı
+            //dur bi dk
+            lblBakiye.Text = "500";
+            if (lblBakiye.Text == "0")
+            {
+                MessageBox.Show("Bakiyenizde Para Kalmamıştır");
+            }
+        }
+
+
+    }
+}
